5355086f018162774c066f27a749c4b78265cd7b
Akhil-Patil-Bagili <akhilpatilbagili@gmail.com>
2024-01-17 12:30:00 +0530

    added backend

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://akhilpatilbagili:Akhil@18133@cluster0.uuvljkz.mongodb.net/")
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos', todoSchema)
+module.exports= {
+    todo
+}
